ca20fe170e19ba6fae497b84ac8cdfbac0a93064
z@kuro <z@kuro.red>
2020-06-15 23:06:23 +0900

    MainWindow を削除して overlay を mainwindow に

--- EasyCapture/MainWindow.xaml.cs
+++ EasyCapture/MainWindow.xaml.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.ComponentModel;
+using System.Drawing;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,25 +10,150 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
 
 namespace EasyCapture
 {
+
   /// <summary>
-  /// Interaction logic for MainWindow.xaml
+  /// Overlay.xaml の相互作用ロジック
   /// </summary>
   public partial class MainWindow : Window
   {
+    private class SelectionBox : INotifyPropertyChanged
+    {
+      public event PropertyChangedEventHandler PropertyChanged;
+      private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+      {
+        if (PropertyChanged != null)
+        {
+          PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+      }
+      private int top;
+      public int Top {
+        get { return top; }
+        set {
+          if (top != value) {
+            top = value;
+            NotifyPropertyChanged();
+          }
+        }
+      }
+      private int left;
+      public int Left
+      {
+        get { return left; }
+        set
+        {
+          if (left != value)
+          {
+            left = value;
+            NotifyPropertyChanged();
+          }
+        }
+      }
+      private int width;
+      public int Width
+      {
+        get { return width; }
+        set
+        {
+          if (width != value)
+          {
+            width = value;
+            NotifyPropertyChanged();
+          }
+        }
+      }
+      private int height;
+      public int Height
+      {
+        get { return height; }
+        set
+        {
+          if (height != value)
+          {
+            height = value;
+            NotifyPropertyChanged();
+          }
+        }
+      }
+
+      private Visibility visibility = Visibility.Hidden;
+      public Visibility Visibility
+      {
+        get { return visibility; }
+        set
+        {
+          if (visibility != value)
+          {
+            visibility = value;
+            NotifyPropertyChanged();
+          }
+        }
+      }
+    }
+
+    private bool selecting {
+      get {
+        return selectionBox.Visibility == Visibility.Hidden;
+      }
+    }
+
+    private readonly SelectionBox selectionBox = new SelectionBox();
+
     public MainWindow()
     {
       InitializeComponent();
+      this.DataContext = new { SelectionBox = this.selectionBox };
     }
 
-    private void OpenOverlay(object sender, RoutedEventArgs e)
+    private void Window_KeyDown(object sender, KeyEventArgs e)
     {
-      var overlay = new Overlay();
-      overlay.Show();
+      if (e.Key == Key.Escape)
+      {
+        this.Close();
+      }
+    }
+
+    private void CancelSelect()
+    {
+      selectionBox.Visibility = Visibility.Hidden;
+    }
+
+    private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+      selectionBox.Visibility = Visibility.Visible;
+      var pos = e.GetPosition(this);
+      selectionBox.Left =(int)pos.X;
+      selectionBox.Top = (int)pos.Y;
+
+    }
+
+    private void Window_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+    {
+      selectionBox.Visibility = Visibility.Hidden;
+
+      using (var image = new Bitmap(selectionBox.Width, selectionBox.Height))
+      {
+        var graphics = Graphics.FromImage(image);
+        graphics.CopyFromScreen(
+          new System.Drawing.Point(0, 0),
+          new System.Drawing.Point(0, 0),
+          new System.Drawing.Size(selectionBox.Width, selectionBox.Height)
+        );
+        image.Save("screenshot.png");
+      }
+    }
+
+    private void Window_MouseMove(object sender, MouseEventArgs e)
+    {
+      var pos =e.GetPosition(this);
+      selectionBox.Width = (int)pos.X - selectionBox.Left;
+      selectionBox.Height = (int)pos.Y - selectionBox.Top;
     }
   }
 }
