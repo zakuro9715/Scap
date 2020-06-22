b7382b12c25147f0f878f3ff60947a702e3f9269
z@kuro <z@kuro.red>
2020-06-23 03:05:18 +0900

    Reafctor

--- EasyCapture/PaintDialog.xaml.cs
+++ EasyCapture/PaintDialog.xaml.cs
@@ -22,41 +22,36 @@ namespace EasyCapture
   public partial class PaintDialog : Window
   {
     public BitmapEncoder Encoder { get; private set; }
-    public PaintDialog(Bitmap originalImage)
+    private Bitmap image;
+
+    public PaintDialog(Bitmap image)
     {
       InitializeComponent();
-      canvas.Height = originalImage.Height;
-      canvas.Width = originalImage.Width;
+
+      this.image = image;
+      canvas.Height = image.Height;
+      canvas.Width = image.Width;
       using (var ms = new MemoryStream())
       {
-        originalImage.Save(ms, ImageFormat.Bmp);
+        image.Save(ms, ImageFormat.Bmp);
         ms.Position = 0;
 
         var brush = new ImageBrush();
-        var image = new BitmapImage();
-        image.BeginInit();
-        image.CacheOption = BitmapCacheOption.OnLoad;
-        image.CreateOptions = BitmapCreateOptions.None;
-        image.StreamSource = ms;
-        image.EndInit();
-        image.Freeze();
-        brush.ImageSource = image;
+        var bg = new BitmapImage();
+        bg.BeginInit();
+        bg.CacheOption = BitmapCacheOption.OnLoad;
+        bg.StreamSource = ms;
+        bg.EndInit();
+        bg.Freeze();
+        brush.ImageSource = bg;
         canvas.Background = brush;
       }
     }
 
     private void WriteImageToSTream()
     {
-      var drawingVisual = new DrawingVisual();
-      var drawingContext = drawingVisual.RenderOpen();
-
-      var rect = new Rect(0, 0, canvas.ActualWidth, canvas.ActualHeight);
-      drawingContext.DrawRectangle(canvas.Background, null, rect);
-      canvas.Strokes.Draw(drawingContext);
-      drawingContext.Close();
-
-      var target = new RenderTargetBitmap((int)rect.Width, (int)rect.Height, 96, 96, PixelFormats.Default);
-      target.Render(drawingVisual);
+      var target = new RenderTargetBitmap((int)canvas.ActualWidth, (int)canvas.ActualHeight, 96, 96, PixelFormats.Default);
+      target.Render(canvas);
 
       Encoder = new PngBitmapEncoder();
       Encoder.Frames.Add(BitmapFrame.Create(target));
